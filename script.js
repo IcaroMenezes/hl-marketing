b14a00693472a6a6a934559fe9b263f10145f377
icaromenezes <icarod.menezes@gmail.com>
2024-06-04 22:28:10 -0300

    inital commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function whatsapp(){
     window.open('https://wa.me/5511947384020?text=Ol%C3%A1%21+Eu+gostaria+de+agendar+uma+reuni%C3%A3o+para+conhecer+os+meios+de+alavancar+as+minhas+vendas+por+meio+de+an%C3%BAncios%21')
 }
+
