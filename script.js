f5c3f21a699e8cbbfc27822a1dab630a332d16b3
icaromenezes <icarod.menezes@gmail.com>
2024-06-05 11:37:57 -0300

    feat: conversão

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function whatsapp(){
-    window.open('https://wa.me/5511947384020?text=Ol%C3%A1%21+Eu+gostaria+de+agendar+uma+reuni%C3%A3o+para+conhecer+os+meios+de+alavancar+as+minhas+vendas+por+meio+de+an%C3%BAncios%21')
+    window.open('https://wa.me/5511947384020?text=Ol%C3%A1%21+Eu+gostaria+de+agendar+uma+reuni%C3%A3o+%21')
 }
 
 document.addEventListener('DOMContentLoaded', function() {
