db74655372e267df48d4e8ff89d8da316237d15b
icaromenezes <icarod.menezes@gmail.com>
2024-06-13 11:44:04 -0300

    feat: carrosel

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ function goToBtn(){
         behavior: 'smooth'
     });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    let currentIndex = 0;
+    const slides = document.querySelectorAll('.slide');
+        function showNextSlide() {
+            slides[currentIndex].classList.remove('active');
+            currentIndex = (currentIndex + 1) % slides.length;
+            slides[currentIndex].classList.add('active');
+        }
+        console.log(currentIndex)
+        setInterval(showNextSlide, 5000);
+        slides[currentIndex].classList.add('active');
+});
+
